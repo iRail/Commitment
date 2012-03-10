7160e8d519f83678914b3640d5b8958362cb3590
Wouter Vandenneucker <wouter@vandenneucker.be>
2012-03-10 16:39:06 +0100

    css updates

--- index.php
+++ index.php
@@ -11,17 +11,44 @@
   <meta name="description" content="">
 
   <meta name="viewport" content="width=device-width">
-  <link rel="stylesheet" href="css/style.css">
-
+  <link rel="stylesheet" href="css/style.css" media="screen" rel="stylesheet" type="text/css" />
+  <link rel="stylesheet" href="css/screen.css" media="screen" rel="stylesheet" type="text/css" />
+ 
   <script src="js/libs/modernizr-2.5.3.min.js"></script>
 </head>
 <body>
   <!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
+	<div class="above"></div>
   <header>
-De Lijn - Nachtbussen <!--Change this text into config later-->
+	<p>De Lijn - Nachtbussen</p> <!--Change <this text into config later-->
+	<div id="left" class="box">
+		<div class="boxtop">Bereik:</div>
+		<div class="boxbottom"><span style="color: #6fafff; font-weight: 900; font-size: 24px">4000</span><br> pers.</div>
+	</div>
+	<div id="right" class="box">
+		<div class="boxtop">Deel online:</div>
+
+<script>function fbs_click() {u=location.href;t=document.title;window.open('http://www.facebook.com/sharer.php?u='+encodeURIComponent(u)+'&t='+encodeURIComponent(t),'sharer','toolbar=0,status=0,width=626,height=436');return false;}</script><a rel="nofollow" href="http://www.facebook.com/share.php?u=<;url>" onclick="return fbs_click()" target="_blank">Share on Facebook</a>
+
+
+		<div class="boxbottom">FB<br><br><a href="http://twitter.com/?status=Ik%20neem%20de%20nachtbus!%20Omdat%20nacht-,%20feest-%20en%20avondlijnen%20moeten%20blijven:%20http%3A%2F%2Fikneemdenachtbus.be%20%23ikneemdenachtbus" target="blank">twitter</a></div>
+	</div>
   </header>
-  <div role="main">
-	TODO
+	<!-- content -->
+  <div role="main" class="content">
+		<div class="contentbox-bar"><h1>Informatie</h1></div>
+		<div class="contentbox">
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+		</div>
+		<div class="contentbox-bar"><h1>Vind je lijn</h1></div>
+		<div class="contentbox">
+			<h2 style="text-align: center; color: #f1e300; font-weight: 900;">iRail API goes here</h2>
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+		</div>
+		<div class="contentbox-bar"><h1>Teken de petitie</h1></div>
+		<div class="contentbox">
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+		</div>
   </div>
   <footer>
 &copy; 2012 iRail vzw/asbl - Some Rights Reserved - <a href="http://github.com/iRail/Commitment">Source code</a>
