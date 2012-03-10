6d28723b863a6302cf358b651d3d703bd13c4966
Wouter Vandenneucker <wouter@vandenneucker.be>
2012-03-11 00:25:25 +0100

    landingpage added // index.php is now home.php

--- index.php
+++ index.php
@@ -14,55 +14,19 @@
   <link rel="stylesheet" href="css/screen.css" media="screen" rel="stylesheet" type="text/css" />
  
   <script src="js/libs/modernizr-2.5.3.min.js"></script>
-
-  <?php require 'dbconfig.php' ?>
 </head>
 <body>
   <!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
 	<div class="above">
 </div>
   <header>
-	<div id="left" class="box">
-		<div class="boxtop">Bereik:</div>
-			<?php
-			$result = mysql_query("SELECT * FROM users", $link);
-			$num_rows = mysql_num_rows($result);
-			?>
-		<div class="boxbottom"><span style="color: #6fafff; font-weight: 900; font-size: 24px"><?php echo $num_rows; ?></span><br> pers.</div>
-	</div>
-	<div id="right" class="box">
-		<div class="boxtop">Deel online:</div>
-		<div class="boxbottom">
-		<a target="_blank" href="http://www.facebook.com/dialog/feed?
-app_id=273909402684521&link=http://ikneemdenachtbus.be&picture=http://ikneemdenachtbus.be/img/logo.png&name=Ik%20neem%20de%20nachtbus!&caption=Omdat%20nacht-,%20feest-%20en%20avondlijnen%20moeten%20blijven&description=Petitie%20tegen%20de%20afschaffing%20van%20de%20nachtlijnen%20en%20voor%20meer%20en%20duidelijke%20informatie.&message=Teken%20nu%20de%20petitie%20voor%20het%20behoud%20van%20de%20nacht-%20en%20avondlijnen!&redirect_uri=http://ikneemdenachtbus.be" title="Deel deze webpagina op Facebook">facebook</a>
-		<br>
-		<a href="http://twitter.com/?status=Ik%20neem%20de%20nachtbus!%20Omdat%20nacht-,%20feest-%20en%20avondlijnen%20moeten%20blijven:%20http%3A%2F%2Fikneemdenachtbus.be%20%23ikneemdenachtbus" target="_blank" title="Deel deze webpagina op Twitter">twitter</a></div>
-	</div>
+
   </header>
 	<!-- content -->
   <div role="main" class="content">
 		<div class="contentbox-bar"><h1>Informatie</h1></div>
 		<div class="contentbox">
-			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-		</div>
-		<div class="contentbox-bar"><h1>Vind je lijn</h1></div>
-		<div class="contentbox">
-			<h2 style="text-align: center; color: #f1e300; font-weight: 900;">iRail API goes here</h2>
-			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-		</div>
-		<div class="contentbox-bar"><h1>Teken de petitie</h1></div>
-		<div class="contentbox">
-			<form method="post" action="sign.php">
-				<div id="signbox">
-					<div class="input-description">Voornaam: <input class="inputbox" type="text" name="fname" />
-					</div>
-					<div class="input-description">Familienaam: <input class="inputbox" type="text" name="lname" />
-					</div>
-					<div class="input-description">Email: <input class="inputbox" type="text" name="email" />
-					</div>
-					<input class="submit" type="submit" name="submit"  value="teken" />
-				</div>
-			</form>
+			<p>Hier komt de reizigerspetitie voor het behoud van de avond-, en nachtlijnen van "De Lijn".<br><br>We zijn op dit moment nog hard aan het werk om alles online te krijgen.<br>Check snel opnieuw want de site komt er zo aan.<br><br>iRail vzw</p>
 		</div>
   </div>
   <footer>
